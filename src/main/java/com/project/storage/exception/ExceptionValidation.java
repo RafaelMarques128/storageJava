f20143cf74834c7443251cb3c1171f7ff6bc9ea4
Rafael Souza Marques <rsmarquesprog@gmail.com>
2024-12-12 15:00:06 -0300

    Criando ExceptionValidation

---
+++ src/main/java/com/project/storage/exception/ExceptionValidation.java
@@ -0,0 +1,9 @@
+package com.project.storage.exception;
+
+public class ExceptionValidation extends RuntimeException{
+
+    public ExceptionValidation(String mensage) {
+        super(mensage);
+    }
+
+}
